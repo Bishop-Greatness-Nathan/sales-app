44b19bac0cd3635aa305739b93b17a942e1e0e85
GreatnessBarthNathan <ekemeziebartholomew@gmail.com>
2025-01-26 19:57:49 +0100

    .

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "development") {
 }
 // app.use(express.static(path.resolve(__dirname, "./public")))
 
-app.use(express.static(path.resolve(__dirname, "../front-end/dist")))
+app.use(express.static(path.resolve(__dirname, "../../front-end/dist")))
 
 app.use(express.json())
 app.use(cookieParser())
@@ -57,7 +57,7 @@ app.use("/api/v1/endofday", authenticateUser, endOfDayRouter)
 //   res.sendFile(path.resolve(__dirname, "./public", "index.html"))
 // })
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../front-end/dist", "index.html"))
+  res.sendFile(path.resolve(__dirname, "../../front-end/dist", "index.html"))
 })
 
 app.use("*", (req, res) => {
