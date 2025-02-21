ea006d125339d59c972ee0c0ff28ffdbdec6683e
GreatnessBarthNathan <ekemeziebartholomew@gmail.com>
2025-02-21 15:51:39 +0100

    removed turbo

---
+++ backend/src/utils/tokenUtils.ts
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
+
+type Payload = {
+  _id: mongoose.Types.ObjectId
+  userName: string
+  role: string
+  approved: boolean
+}
+
+export const createJwt = (payload: Payload) => {
+  return jwt.sign(payload, process.env.JWT_SECRET as string)
+}
+
+export const verifyJwt = (token: string) => {
+  return jwt.verify(token, process.env.JWT_SECRET as string)
+}
