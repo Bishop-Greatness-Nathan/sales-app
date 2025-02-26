a859e9ebb6f2f995ebfc9db33ba5e0aeca5bc40b
GreatnessBarthNathan <ekemeziebartholomew@gmail.com>
2025-02-26 11:03:19 +0100

    .

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -32,9 +32,9 @@ import endOfDayRouter from "./routes/endOfDayRoutes"
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"))
 }
-// app.use(express.static(path.resolve(__dirname, "./public")))
+app.use(express.static(path.resolve(__dirname, "./public")))
 
-app.use(express.static(path.resolve(__dirname, "../../front-end/dist")))
+// app.use(express.static(path.resolve(__dirname, "../../front-end/dist")))
 
 app.use(express.json())
 app.use(cookieParser())
@@ -53,12 +53,12 @@ app.use("/api/v1/bank", authenticateUser, bankRouter)
 app.use("/api/v1/category", authenticateUser, categoryRouter)
 app.use("/api/v1/endofday", authenticateUser, endOfDayRouter)
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./public", "index.html"))
-// })
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../../front-end/dist", "index.html"))
+  res.sendFile(path.resolve(__dirname, "./public", "index.html"))
 })
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../../front-end/dist", "index.html"))
+// })
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" })
