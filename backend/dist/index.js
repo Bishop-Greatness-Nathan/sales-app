e1cd940f76d599c63d1ab0b9efff9c53e1aa5803
GreatnessBarthNathan <ekemeziebartholomew@gmail.com>
2025-02-26 11:37:22 +0100

    .

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -51,7 +51,6 @@ const dotenv = __importStar(require("dotenv"));
 const mongoose_1 = __importDefault(require("mongoose"));
 dotenv.config();
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
-const path_1 = __importDefault(require("path"));
 const morgan_1 = __importDefault(require("morgan"));
 const helmet_1 = __importDefault(require("helmet"));
 const express_mongo_sanitize_1 = __importDefault(require("express-mongo-sanitize"));
@@ -73,7 +72,7 @@ const endOfDayRoutes_1 = __importDefault(require("./routes/endOfDayRoutes"));
 if (process.env.NODE_ENV === "development") {
     app.use((0, morgan_1.default)("dev"));
 }
-app.use(express_1.default.static(path_1.default.resolve(__dirname, "./public")));
+// app.use(express.static(path.resolve(__dirname, "./public")))
 // app.use(express.static(path.resolve(__dirname, "../../front-end/dist")))
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
@@ -90,15 +89,15 @@ app.use("/api/v1/cash", authMiddleware_1.authenticateUser, cashRoutes_1.default)
 app.use("/api/v1/bank", authMiddleware_1.authenticateUser, bankRoutes_1.default);
 app.use("/api/v1/category", authMiddleware_1.authenticateUser, categoryRoutes_1.default);
 app.use("/api/v1/endofday", authMiddleware_1.authenticateUser, endOfDayRoutes_1.default);
-app.get("*", (req, res) => {
-    res.sendFile(path_1.default.resolve(__dirname, "./public", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./public", "index.html"))
+// })
 // app.get("*", (req, res) => {
 //   res.sendFile(path.resolve(__dirname, "../../front-end/dist", "index.html"))
 // })
-app.use("*", (req, res) => {
-    res.status(404).json({ msg: "not found" });
-});
+// app.use("*", (req, res) => {
+//   res.status(404).json({ msg: "not found" })
+// })
 // app.use(notFoundError)
 app.use(errorHandler_1.default);
 const port = Number(process.env.PORT) || 4000;
@@ -106,7 +105,7 @@ const host = process.env.HOST || "0.0.0.0";
 const start = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         yield mongoose_1.default.connect(process.env.MONGO_URL);
-        app.listen(port, host, () => __awaiter(void 0, void 0, void 0, function* () {
+        app.listen(port, () => __awaiter(void 0, void 0, void 0, function* () {
             console.log("connected to DB");
             console.log(`server is listening on port ${port}`);
         }));
